d492e77e89ca77b7fdf6ff362406646b79c6d8de
Mikita Krahelski <nikita.krahelski@gmail.com>
2021-02-16 14:32:37 -0800

    Removed more superflous commentary, and cleaned up the file structure for readability.

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,8 +11,6 @@ import mongoose from 'mongoose';
 
 */
 const userSchema = new mongoose.Schema({
-    //can't attach a validator it breaks :)
-    //I wasn't adding type instead I just said string :)
     FirstName: { type: String, required: true,},
     LastName: { type: String, required: true,},
     Permissions: { type: String, }, //will be an array of strings
@@ -27,12 +25,7 @@ const userSchema = new mongoose.Schema({
     { collection : 'test' }
     
     );
-    
         
-    //it worked I think?
     const User : mongoose.Model<any> = mongoose.model('User', userSchema);
-    export = User;
-
-    //is : mongoose.model<any> the type declaration needed here
-    
+    export = User;    
     
